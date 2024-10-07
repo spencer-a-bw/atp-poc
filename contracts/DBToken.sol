518c42cdfc09d6e18018cd5816c0ceff729f588e
Spencer Clarke <sclarke@Spencers-MacBook-Pro.local>
2024-10-07 14:53:53 -0400

    Update smart contract

--- contracts/DBToken.sol
+++ contracts/DBToken.sol
@@ -13,10 +13,10 @@
 pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DBToken is ERC20, ERC20Burnable, Ownable {
+contract DBToken is ERC20, Ownable {
 
     mapping(address => bool) public generalWhitelist;
 
